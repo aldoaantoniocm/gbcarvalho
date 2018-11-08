85bedf3f1d23d2f3091ab4de91c207d6d61c07cc
gabrielbatistacarvalho <44482023+gabrielbatistacarvalho@users.noreply.github.com>
2018-11-08 19:31:05 -0200

    Add files via upload

--- manutencao.sql
+++ manutencao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Out-2018 às 20:22
+-- Generation Time: 08-Nov-2018 às 22:29
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.10
 
@@ -36,16 +36,31 @@ CREATE TABLE `conserto` (
   `data_entrada` date NOT NULL,
   `data_saida` date DEFAULT NULL,
   `defeito` varchar(60) NOT NULL,
-  `conserto` varchar(60) DEFAULT NULL
+  `reparo` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `conserto`
 --
 
-INSERT INTO `conserto` (`id`, `patri_maq`, `local`, `est_resp`, `data_entrada`, `data_saida`, `defeito`, `conserto`) VALUES
-(1, 454567, 1, 1, '2018-10-10', NULL, 'teste', NULL),
-(2, 454567, 1, 1, '2018-10-10', NULL, 'teste', NULL);
+INSERT INTO `conserto` (`id`, `patri_maq`, `local`, `est_resp`, `data_entrada`, `data_saida`, `defeito`, `reparo`) VALUES
+(2, 456776, 5, 1, '2018-10-10', NULL, 'monitor fica rosa', 'troca de lcd'),
+(6, 454567, 14, 16, '2018-10-10', NULL, 'reinicia a cada meia hora', ''),
+(7, 576457, 14, 1, '2018-10-17', NULL, 'tela azul', ''),
+(8, 252627, 1, 1, '2018-10-17', NULL, 'tela azul', ''),
+(9, 576457, 4, 2, '2018-10-19', NULL, 'botÃ£o de ligar quebrado', ''),
+(10, 576457, 2, 3, '2018-10-19', NULL, 'botÃ£o de ligar quebrado', 'trocou botÃ£o de ligar'),
+(11, 576457, 3, 1, '2018-11-21', NULL, 'queimou a fonte', ''),
+(12, 576457, 3, 1, '2018-11-21', NULL, 'queimou a fonte', ''),
+(13, 456776, 3, 1, '2018-10-19', NULL, 'tela azul', ''),
+(14, 252627, 1, 1, '2018-10-19', NULL, 'queimou a fonte', ''),
+(15, 456776, 13, 16, '2018-10-17', NULL, 'nÃ£o liga', 'trocou botÃ£o de ligar'),
+(16, 252627, 3, 2, '2018-10-19', NULL, 'nÃ£o da video', ''),
+(17, 576457, 4, 2, '2018-10-23', '2018-11-01', 'botÃ£o de ligar quebrado', ''),
+(18, 252627, 4, 3, '2018-10-24', '2018-11-01', 'nÃ£o liga', ''),
+(22, 252627, 13, 16, '2018-10-24', '2018-11-01', 'botÃ£o de ligar quebrado', ''),
+(23, 252627, 1, 1, '2018-10-24', NULL, 'botÃ£o de ligar quebrado', ''),
+(25, 252627, 1, 2, '2018-10-25', NULL, 'nÃ£o liga', '');
 
 -- --------------------------------------------------------
 
@@ -55,18 +70,25 @@ INSERT INTO `conserto` (`id`, `patri_maq`, `local`, `est_resp`, `data_entrada`, 
 
 CREATE TABLE `estagiario` (
   `id` int(11) NOT NULL,
-  `nome` varchar(60) NOT NULL
+  `nome` varchar(60) NOT NULL,
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `estagiario`
 --
 
-INSERT INTO `estagiario` (`id`, `nome`) VALUES
-(1, 'Gabriel batista de Carvalho'),
-(2, 'Rodolfo'),
-(3, 'Higor'),
-(4, 'Pablo');
+INSERT INTO `estagiario` (`id`, `nome`, `status`) VALUES
+(1, 'Gabriel ', 1),
+(2, 'Rodolfo', 0),
+(3, 'Higor', 1),
+(4, 'Pablo', 0),
+(16, 'joao', 1),
+(17, 'lucas', 0),
+(18, 'lucas', 0),
+(19, 'lucas', 0),
+(20, 'carla', 0),
+(21, 'carol', 1);
 
 -- --------------------------------------------------------
 
@@ -88,7 +110,9 @@ INSERT INTO `local` (`id`, `predio`) VALUES
 (2, 'Prédio 2'),
 (3, 'Prédio de História'),
 (4, 'Prédio de Psicologia'),
-(5, 'Prédio Nova Central');
+(5, 'Prédio Nova Central'),
+(13, 'CPPA'),
+(14, 'CEDAP');
 
 -- --------------------------------------------------------
 
@@ -97,9 +121,20 @@ INSERT INTO `local` (`id`, `predio`) VALUES
 --
 
 CREATE TABLE `login` (
-  `email` varchar(30) NOT NULL,
-  `senha` varchar(20) NOT NULL
+  `usuario` varchar(10) NOT NULL,
+  `senha` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `login`
+--
+
+INSERT INTO `login` (`usuario`, `senha`) VALUES
+('Aldo Anton', '698d51a19d8a121ce581499d7b701668'),
+('Aldocm', '202cb962ac59075b964b07152d234b70'),
+('gabriel', '202cb962ac59075b964b07152d234b70'),
+('Natane', 'a09b68ada5e8b2060f380a53c02cf3d8'),
+('wadoryu', 'e10adc3949ba59abbe56e057f20f883e');
 
 -- --------------------------------------------------------
 
@@ -120,8 +155,10 @@ CREATE TABLE `maquina` (
 --
 
 INSERT INTO `maquina` (`patrimonio`, `processador`, `mem_ram`, `hd`, `mac`) VALUES
-(454567, 'i5', '2gb', '1tb', 'ha-3h-y4-hs-83-js-83'),
-(456776, 'i7', '16gb', '2tb', 'hg-76-8u-oi-9o-09-hf');
+(252627, 'i5', '8gb', '1tb', '16-jf-o9-o8-t6-yt-76'),
+(454567, 'i3', '4gb', '500gb', 'ha-3h-y4-hs-83-js-84'),
+(456776, 'i7', '16gb', '2tb', 'hg-76-8u-oi-9o-09-hf'),
+(576457, 'i7', '16', '2tb', '17-su-u8-87-jh-98-9i');
 
 --
 -- Indexes for dumped tables
@@ -153,7 +190,7 @@ ALTER TABLE `local`
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`usuario`);
 
 --
 -- Indexes for table `maquina`
@@ -169,25 +206,25 @@ ALTER TABLE `maquina`
 -- AUTO_INCREMENT for table `conserto`
 --
 ALTER TABLE `conserto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `estagiario`
 --
 ALTER TABLE `estagiario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `local`
 --
 ALTER TABLE `local`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `maquina`
 --
 ALTER TABLE `maquina`
-  MODIFY `patrimonio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=456777;
+  MODIFY `patrimonio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=576458;
 
 --
 -- Constraints for dumped tables
